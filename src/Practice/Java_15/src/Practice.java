c95f26dc9c771767c84e78dc8d92141ee1f0b2b9
ssavassava <20160658@gm.hannam.ac.kr>
2022-09-04 22:21:01 +0900

    finish Java_15

--- src/Practice/Java_15/src/Practice.java
+++ src/Practice/Java_15/src/Practice.java
@@ -1,9 +1,25 @@
 package Practice.Java_15.src;// Practice
 // 로또 번호 만들기
 
-public class Practice {
-    public static void main(String[] args) {
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
 
+public class Practice {
+
+    public static void main(String[] args) {
+        HashSet set = new HashSet();
+
+        for (int i = 0; set.size() < 6; i++) {
+            int num = (int)(Math.random() * 45) + 1;
+            set.add(num);
+        }
+
+        System.out.println("set = " + set);
+
+        LinkedList list = new LinkedList(set);
+        Collections.sort(list);
+        System.out.println("로또 번호: " + list);
 
     }
 }
