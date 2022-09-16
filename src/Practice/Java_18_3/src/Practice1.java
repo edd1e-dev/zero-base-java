140d1ba1af21e179867f6253c0c631347ea0c9d1
edd1e-dev <20160658@gm.hannam.ac.kr>
2022-09-16 21:10:07 +0900

    fix Java_18_3

--- src/Practice/Java_18_3/src/Practice1.java
+++ src/Practice/Java_18_3/src/Practice1.java
@@ -2,7 +2,21 @@ package Practice.Java_18_3.src;
 
 public class Practice1 {
     public static void solution(int[] nums) {
+        int idx = 0;
+        for (int num : nums) {
+            if (idx == 0 || num > nums[idx - 1]) {
+                nums[idx++] = num;
+            }
+        }
 
+        for (int i = 0; i < idx;) {
+            System.out.print(nums[i]);
+            if (++i < idx) {
+                System.out.print(",");
+            }
+        }
+
+        System.out.println();
     }
 
     public static void main(String[] args) {
